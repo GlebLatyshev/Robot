21586504e3d488b20264b37f84ebd8ab436346da
Латышев Глеб <homerunjr57@stud.vvsu.ru>
2025-05-15 16:04:21 +1000

    2st deploy

---
+++ src/build/QRcode.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class = "qr-wrapper">
+        <h2>QR-код на портфолио</h2>
+        <canvas ref="canvas"></canvas>
+        <p>Отсканируй, чтобы открыть</p>
+        <button class = "visit-btn" @click="visitSite">Перейти по QR - коду</button>
+
+    </div>
+</template>
+
+<script>
+import QRcode from 'qrcode'
+export default{
+    name: 'QRcode', 
+    data(){
+        return{
+            url: 'https://gleblatyshev.github.io/Robot/' //Указываем нужную ссылку
+        }
+    },
+    mounted(){
+        const canvas = this.$refs.canvas
+        if(canvas){
+            QRcode.toCanvas(canvas, this.url, {wigth: 180}, (error) =>{
+                if (error) console.error(error)
+            })
+        }
+    },
+    methods: {
+        visitSite(){
+            window.open(this.url, '_blank')
+        }
+    }
+}
+</script>
